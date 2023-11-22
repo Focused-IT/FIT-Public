e805dd0bfb6fd0b0012a06f62f574ff38291d10b
Simon Godefroy <30377539+Jersi132@users.noreply.github.com>
2023-11-22 17:04:40 +0000

    Create Test-Script.ps1

---
+++ Test-Script.ps1
@@ -0,0 +1,22 @@
+<#
+.SYNOPSIS 
+	Test Script.
+.DESCRIPTION 
+	Test Script
+.EXAMPLE
+	Test-Script
+.NOTES 
+	Author		: Sean Hamon - Sean.Hamon@FocusedIT.co.uk
+	Version		: 1.0.1
+	Date		: 2018.03.22
+		Update	:	1.0.1
+					SH - 2018.03.22
+					Test Update
+		Update	:	1.0.0
+					SH - 2018.03.22
+					Initial Script
+.LINK 
+	http://www.FocusedIT.co.uk
+#> 
+
+Write-Host "Hello World"
